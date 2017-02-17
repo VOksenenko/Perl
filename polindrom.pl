e272274ffeecdc0a8277968fca8e76f1fe4271d1
VOksenenko <voksenenko@gmail.com>
2017-02-17 17:19:59 +0200

    reverse test

--- polindrom.pl
+++ polindrom.pl
@@ -1,11 +1,12 @@
 #!/usr/bin/perl -w
 use strict;
 
-open (my $text, "<", "words") or die "Can't open text file!";
+#open (my $text, "<", "words") or die "Can't open text file!";
 
-#while(
-    my $line = <$text>;
-    print "$line";
-#}
+#while(<>){
+    my $line = <STDIN>;
+    my $line2 = reverse ($line);
+    print ("$line2\n");
+
 
 
