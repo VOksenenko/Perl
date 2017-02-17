16ad4d13bb4458b353282587c80a4a42fecbf7b0
VOksenenko <voksenenko@gmail.com>
2017-02-17 18:54:44 +0200

    one character is polindrome too.

--- polindrom.pl
+++ polindrom.pl
@@ -5,15 +5,13 @@ my @polinroms;
 my $counter = 0;
 open (my $text, "<", "words") or die "Can't open text file!";
 
-while(my $line = <$text>) {
+while (my $line = <$text>) {
     chomp $line;
-    if(length ($line) >= 2){
         if ($line eq reverse ($line)) {
             push (@polinroms, $line);
             $counter++;
         }
-    }
 }
-print "I have found $counter polindroms: \n\n";
-print "@polinroms\n";
+print "I have found $counter polindroms.\n";
+#print "@polinroms\n";
 
