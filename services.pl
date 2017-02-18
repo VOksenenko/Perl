3ba23ce34e5532dad918ef4874932e6ab6244284
VOksenenko <voksenenko@gmail.com>
2017-02-18 14:14:12 +0200

    test comment lines detect

--- services.pl
+++ services.pl
@@ -1,6 +1,23 @@
 #!/usr/bin/perl -w
 use strict;
 
+#Create a program which returns a service name after giving it a port number. A file /etc/services should be used. Usage:
+#
+#program_name first_port [last_port]
+#
+#If a second port is mentioned in an argument list, the program should print all services that have port numbers in that range.
+#An output should look like this:
+#
+#port_number - service.
+#
+#The program has to process exceptions like blank lines or comments in the file and handle an incorrect input from a user.
+
 open (my $services, "<", "services") or die "Can't open file!";
-print while (<$services>) ;
-print "@ARGV\n";
+
+#print while (<$services>) ;
+my $line = <$services>;
+chomp $line;
+
+if ( (substr($line, 0,1)) eq "#" ){
+    print "Comment\n";
+}
