21e7601c4d7c7ef85dd7cbb2c3de5af2f09221c2
VOksenenko <voksenenko@gmail.com>
2017-02-19 00:01:32 +0200

    added checking if argument2 is greater then argument 1 and conditions refactoring

--- services.pl
+++ services.pl
@@ -11,15 +11,16 @@ use strict;
 #port_number - service.
 #
 #The program has to process exceptions like blank lines or comments in the file and handle an incorrect input from a user.
+if ( scalar(@ARGV) < 1 or  scalar(@ARGV) > 2){
+    print "\nUsage: ./services.pl port [port2]\n\n";    
+} elsif (scalar(@ARGV) == 2 and $ARGV[0] > $ARGV[1]) {
+    print "Argument2 should be greater then argument 1!\n";
+} else {
 
-open (my $file, "<", "services") or die "Can't open file!";
-my %services;
-my $P1;
-my $P2;
-
-if ( scalar(@ARGV) > 0 and  scalar(@ARGV) < 3){
-    $P1 = $ARGV[0]; 
-    $P2 = $ARGV[1] if ( scalar(@ARGV) == 2 );
+    open (my $file, "<", "services") or die "Can't open file!";
+    my $P1 = $ARGV[0];
+    my $P2 = $ARGV[1] if ( scalar(@ARGV) == 2);
+    my %services;
     
     while (my $line = <$file>) {
         chomp $line;
@@ -52,7 +53,4 @@ if ( scalar(@ARGV) > 0 and  scalar(@ARGV) < 3){
             print "$P1 - no assigned service\n";
         }
     }
-    
-} else {
-    print "\nUsage: ./services.pl port [port2]\n\n";
-}
+} 
