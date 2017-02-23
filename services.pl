12949c91c34ba91ef02808d9b8db06e38114b167
VOksenenko <voksenenko@gmail.com>
2017-02-23 12:23:15 +0200

    added checking for arguments are numbers

--- services.pl
+++ services.pl
@@ -1,6 +1,17 @@
 #!/usr/bin/perl -w
 use strict;
+
 my $usage = "\nUsage: ./services.pl port [port2]\n\n";
+my $message = "\nBoth arguments should be numbers!\n\n";
+
 die "$usage" unless (@ARGV and @ARGV <= 2);
-my ($port1, $port2) = sort {$a <=> $b} @ARGV;
-print "$port1 - $port2\n";
+my ($port1, $port2) = @ARGV;
+#print "$port1, $port2\n";
+
+no warnings;
+die "$message" if ( ($port1 + 0) ne $port1);
+die "$message" if ( $port2 and ($port2 + 0) ne $port2 );
+use warnings;
+
+
+#my ($port1, $port2) = sort {$a <=> $b} @ARGV;
