b674f30c6833a13476c44504c43a6be5633d9b4e
VOksenenko <voksenenko@gmail.com>
2017-03-12 19:14:47 +0200

    added comments

--- Lab6.pl
+++ Lab6.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 use warnings;
 
-#There are several arrays, each element of which is a word. Create a function which finds the longest word(s) from each given array and returns the result as a list. References to arrays should be passed as arguments to the function.
+# There are several arrays, each element of which is a word. Create a function which finds the longest word(s) from each given array and returns the result as a list. References to arrays should be passed as arguments to the function.
 
 my @arr1 = qw /time be different good  person have new /;
 my @arr2 = qw /say year do  government first way  say/;
@@ -10,21 +10,26 @@ my @arr4 = qw /make important great man go little/;
 
 sub longest {
     my  @refarr =  @_;
-    #print "@{$refarr[0]} \n" ; 
     my $max = 0;
-    my @longest; 
-    for (my $i = 0, my $n = @refarr; $i < $n; $i++) { 
+    my @longest;
+    
+    # Во внешнем цикле берем по очереди каждый из переданных массивов в  @refarr
+    for (my $i = 0, my $n = @refarr; $i < $n; $i++) {
+        # Тут проходим по каждому значению конкретного массива  @refarr[i] 
         for my $elem (@{$refarr[$i]}) {
+            # Если каждое следующее слово больше предыдущего, @longest очищается и в него добавляется новое слово максимальной длинны.
             if ( length($elem) > $max ) {
                 $max = length($elem);
                 @longest = ();
-                push @longest, $elem;        
+                push @longest, $elem; 
+            # Если слово такой же длинны, что и  $max, оно добавляется в массив.      
             } elsif (length($elem) == $max) {
                 push @longest, $elem; 
             }
             
         }
     print ("Array", $i+1, ": @longest;\n"); 
+    # После каждого прохода сбрасываем значения.
     $max = 0;
     @longest = ();
     }      
